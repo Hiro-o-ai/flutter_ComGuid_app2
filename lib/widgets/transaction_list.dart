9f3b2c6ccf0c046c911e4c01323a7eca447256e8
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2021-03-02 22:06:34 +0900

    リストが空の時に画像を表示

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -15,58 +15,78 @@ class TransactionList extends StatelessWidget {
       // ListViewはColumnとSingleChildScrollViewを合わせたもの
       // ただし、親のContainerがないとリストは表示されないし、エラーとなる
       // なぜならListViewは画面全体に表示させようとするが、画面上では上にwidgetが存在するため、画面サイズよりも大きくなるため
-      child: ListView.builder(
-        // itemBuilderがないと膨大な量があることを前提に動くので、それを防ぐため
-        itemBuilder: (ctx, index) {
-          return Card(
-            child: Row(
+      child: transactions.isEmpty
+          ? Column(
               children: <Widget>[
+                Text(
+                  'No transactions added yet!',
+                  style: Theme.of(context).textTheme.title,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
                 Container(
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Theme.of(context).primaryColor,
-                      width: 2,
-                    ),
-                  ),
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    // '\$' + tx.amount.toString(),
-                    '\$ ${transactions[index].amount.toStringAsFixed(2)}',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      // Them.of(context).primaryColorで親widgetに設定した色を使用する
-                      // ただし、しようしない場合は黒
-                      // primaryColorDarkなどでprimaryColorの濃淡が変わる
-                      color: Theme.of(context).primaryColor,
-                    ),
+                  height: 200,
+                  child: Image.asset(
+                    'assets/image/waiting.png',
+                    fit: BoxFit.cover,
                   ),
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(transactions[index].title,
-                        // textthmeのtitle:を使用
-                        style: Theme.of(context).textTheme.title
-                        // TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                        ),
-                    Text(
-                      // intlpacage
-                      // pattern1
-                      // DateFormat('yyyy/MM/dd').format(tx.date),
-                      // pattern2
-                      DateFormat.yMMMd().format(transactions[index].date),
-                      style: TextStyle(color: Colors.grey),
-                    ),
-                  ],
-                )
               ],
+            )
+          : ListView.builder(
+              // itemBuilderがないと膨大な量があることを前提に動くので、それを防ぐため
+              itemBuilder: (ctx, index) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Theme.of(context).primaryColor,
+                            width: 2,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          // '\$' + tx.amount.toString(),
+                          '\$ ${transactions[index].amount.toStringAsFixed(2)}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            // Them.of(context).primaryColorで親widgetに設定した色を使用する
+                            // ただし、しようしない場合は黒
+                            // primaryColorDarkなどでprimaryColorの濃淡が変わる
+                            color: Theme.of(context).primaryColor,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(transactions[index].title,
+                              // textthmeのtitle:を使用
+                              style: Theme.of(context).textTheme.title
+                              // TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                              ),
+                          Text(
+                            // intlpacage
+                            // pattern1
+                            // DateFormat('yyyy/MM/dd').format(tx.date),
+                            // pattern2
+                            DateFormat.yMMMd().format(transactions[index].date),
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              },
+              itemCount: transactions.length,
             ),
-          );
-        },
-        itemCount: transactions.length,
-      ),
     );
   }
 }
