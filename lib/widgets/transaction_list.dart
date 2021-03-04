d3ca1c343ee9c96b07da1284be77663909962d38
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2021-03-05 00:08:46 +0900

    circleavaterの書き換え

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -44,8 +44,15 @@ class TransactionList extends StatelessWidget {
                     horizontal: 5,
                   ),
                   child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
+                    // leading: CircleAvatar(
+                    // radius: 30,
+                    leading: Container(
+                      height: 60,
+                      width: 60,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        shape: BoxShape.circle,
+                      ),
                       child: Padding(
                         padding: EdgeInsets.all(6),
                         child: FittedBox(
