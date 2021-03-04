7c5eeaa2561743eee0db6946d7cc5eac6d5cfe44
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2021-03-04 23:52:08 +0900

    リスト表の形を変更

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -38,52 +38,76 @@ class TransactionList extends StatelessWidget {
               // itemBuilderがないと膨大な量があることを前提に動くので、それを防ぐため
               itemBuilder: (ctx, index) {
                 return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Theme.of(context).primaryColor,
-                            width: 2,
-                          ),
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          // '\$' + tx.amount.toString(),
-                          '\$ ${transactions[index].amount.toStringAsFixed(2)}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            // Them.of(context).primaryColorで親widgetに設定した色を使用する
-                            // ただし、しようしない場合は黒
-                            // primaryColorDarkなどでprimaryColorの濃淡が変わる
-                            color: Theme.of(context).primaryColor,
-                          ),
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 5,
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: EdgeInsets.all(6),
+                        child: FittedBox(
+                          child: Text(
+                              '\$ ${transactions[index].amount.toStringAsFixed(2)}'),
                         ),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(transactions[index].title,
-                              // textthmeのtitle:を使用
-                              style: Theme.of(context).textTheme.title
-                              // TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                              ),
-                          Text(
-                            // intlpacage
-                            // pattern1
-                            // DateFormat('yyyy/MM/dd').format(tx.date),
-                            // pattern2
-                            DateFormat.yMMMd().format(transactions[index].date),
-                            style: TextStyle(color: Colors.grey),
-                          ),
-                        ],
-                      )
-                    ],
+                    ),
+                    title: Text(transactions[index].title,
+                        style: Theme.of(context).textTheme.title),
+                    subtitle: Text(
+                      DateFormat.yMMMd().format(transactions[index].date),
+                    ),
                   ),
                 );
+                // return Card(
+                //   child: Row(
+                //     children: <Widget>[
+                //       Container(
+                //         margin:
+                //             EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                //         decoration: BoxDecoration(
+                //           border: Border.all(
+                //             color: Theme.of(context).primaryColor,
+                //             width: 2,
+                //           ),
+                //         ),
+                //         padding: EdgeInsets.all(10),
+                //         child: Text(
+                //           // '\$' + tx.amount.toString(),
+                //           '\$ ${transactions[index].amount.toStringAsFixed(2)}',
+                //           style: TextStyle(
+                //             fontWeight: FontWeight.bold,
+                //             fontSize: 20,
+                //             // Them.of(context).primaryColorで親widgetに設定した色を使用する
+                //             // ただし、しようしない場合は黒
+                //             // primaryColorDarkなどでprimaryColorの濃淡が変わる
+                //             color: Theme.of(context).primaryColor,
+                //           ),
+                //         ),
+                //       ),
+                //       Column(
+                //         crossAxisAlignment: CrossAxisAlignment.start,
+                //         children: <Widget>[
+                //           Text(transactions[index].title,
+                //               // textthmeのtitle:を使用
+                //               style: Theme.of(context).textTheme.title
+                //               // TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                //               ),
+                //           Text(
+                //             // intlpacage
+                //             // pattern1
+                //             // DateFormat('yyyy/MM/dd').format(tx.date),
+                //             // pattern2
+                //             DateFormat.yMMMd().format(transactions[index].date),
+                //             style: TextStyle(color: Colors.grey),
+                //           ),
+                //         ],
+                //       )
+                //     ],
+                //   ),
+                // );
               },
               itemCount: transactions.length,
             ),
