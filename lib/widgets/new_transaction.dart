977d94dea132b4d14192b5a7bd5dc312d1f8e4e0
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2021-03-09 00:16:13 +0900

    追加用モーダルのpadding変更

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -51,67 +51,74 @@ class _NewTransactionState extends State<NewTransaction> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      // CardWidget use only margin not padding
-      // margin: EdgeInsets.all(10),
-      elevation: 5,
-      // ColumnWidget is wraped ContainerWidget because CardWidget and ColumnWidget isn't userful margin and padding
-      // We wanna put space between CardWidget and TextFieldWidget
-      child: Container(
-        padding: EdgeInsets.all(10.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          // ColumnWidget isn't use margin and padding
-          children: <Widget>[
-            TextField(
-              // InputDecoration is many propaty for example add varidation etc
-              decoration: InputDecoration(labelText: 'Title'),
-              controller: _titleController,
-              onSubmitted: (_) => _submitData,
-              // onChanged: (val) {
-              //   titleInput = val;
-              // },
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: 'Amount'),
-              controller: _amountController,
-              // In iOS is not allow decimalplaces. if use it, you select TextInputType.numberWithOptions(decimal: true)
-              // so if you wanna make android and iOS application, you selected numberWith ...
-              keyboardType: TextInputType.numberWithOptions(decimal: true),
-              onSubmitted: (_) => _submitData,
-              // onChanged: (val) => amountInput = val,
-            ),
-            Container(
-              height: 70,
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Text(
-                      _selectedDate == null
-                          ? 'No Date Chosen!'
-                          : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}',
-                    ),
-                  ),
-                  FlatButton(
-                    textColor: Theme.of(context).primaryColor,
-                    onPressed: _presentDatePicker,
-                    child: Text(
-                      'Choose Date',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
+    return SingleChildScrollView(
+      child: Card(
+        // CardWidget use only margin not padding
+        // margin: EdgeInsets.all(10),
+        elevation: 5,
+        // ColumnWidget is wraped ContainerWidget because CardWidget and ColumnWidget isn't userful margin and padding
+        // We wanna put space between CardWidget and TextFieldWidget
+        child: Container(
+          padding: EdgeInsets.only(
+            top: 10,
+            left: 10,
+            right: 10,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 10,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            // ColumnWidget isn't use margin and padding
+            children: <Widget>[
+              TextField(
+                // InputDecoration is many propaty for example add varidation etc
+                decoration: InputDecoration(labelText: 'Title'),
+                controller: _titleController,
+                onSubmitted: (_) => _submitData,
+                // onChanged: (val) {
+                //   titleInput = val;
+                // },
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Amount'),
+                controller: _amountController,
+                // In iOS is not allow decimalplaces. if use it, you select TextInputType.numberWithOptions(decimal: true)
+                // so if you wanna make android and iOS application, you selected numberWith ...
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+                onSubmitted: (_) => _submitData,
+                // onChanged: (val) => amountInput = val,
+              ),
+              Container(
+                height: 70,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Text(
+                        _selectedDate == null
+                            ? 'No Date Chosen!'
+                            : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}',
                       ),
                     ),
-                  ),
-                ],
+                    FlatButton(
+                      textColor: Theme.of(context).primaryColor,
+                      onPressed: _presentDatePicker,
+                      child: Text(
+                        'Choose Date',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            RaisedButton(
-              child: Text('Add Transaction'),
-              color: Theme.of(context).primaryColor,
-              textColor: Theme.of(context).textTheme.button.color,
-              onPressed: _submitData,
-            )
-          ],
+              RaisedButton(
+                child: Text('Add Transaction'),
+                color: Theme.of(context).primaryColor,
+                textColor: Theme.of(context).textTheme.button.color,
+                onPressed: _submitData,
+              )
+            ],
+          ),
         ),
       ),
     );
